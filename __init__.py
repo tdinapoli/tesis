c3faf4bebecab9f0ec9cbdae132afd55a0440c0c
tdinapoli <tomas.dina98@gmail.com>
2023-04-28 17:08:51 -0300

    arranco driver A4988

--- __init__.py
+++ __init__.py
@@ -1,5 +1,20 @@
 
-def TTL:
+class A4988:
+    enable: int
+    a1: int
+    a2: int
+    b1: int
+    b2: int
+    ms1: int
+    ms2: int
+    ms3: int
+    notreset: int
+    notsleep: int
+    step: int
+    dir: int
+
+
+class TTL:
     def __init__(self, GPIO):
         self.gpio = GPIO
 
@@ -14,5 +29,6 @@ class Motor:
     def set_direction(self, direction: bool):
         self.direction_pin.write(direction)
 
-    def step
+    def step(self, direction):
+
 

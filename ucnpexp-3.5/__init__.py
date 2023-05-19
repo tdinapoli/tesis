1533483c9c44c82b2616a7386e343543f6363d12
tdinapoli <tomas.dina98@gmail.com>
2023-05-19 12:18:20 -0300

    create python 3.5 compatible package

---
+++ ucnpexp-3.5/__init__.py
@@ -0,0 +1,368 @@
+# en la rp debe ser:
+# from instruments import abstract
+import abstract
+import yaml
+
+class RPTTL(abstract.TTL):
+    def __init__(self, state, pin, gpio):
+        column, number = pin
+        self._gpio = gpio(column, number, 'out')
+        super().__init__(state)
+        
+    def _get_state(self):
+        return self._gpio.read()
+        
+    def _set_state(self, state):
+        self._gpio.write(state)
+
+class A4988(abstract.MotorDriver):
+    _MODES = (
+                (False, False, False), #Full step
+                (True, False, False) , #Half 
+                (False, True, False) , #Quarter
+                (True, True, False)  , #Eighth
+                (True, True, True)   , #Sixteenth
+               )
+
+    def __init__(self, ttls, mode = 0):
+        for ttl in ttls:
+            setattr(self, ttl, ttls[ttl])
+
+    def set_stepping(self, mode):
+        self.ms1.state, self.ms2.state, self.ms3.state = self._MODES[mode]
+
+    def get_stepping(self):
+        return self._MODES.index((self.ms1.state, self.ms2.state, self.ms3.state))
+
+    #Obs: creo que la minima duracion del pulso es 1 micro segundo
+    #Obs2: para q funcione el step debe ir de low a high. Si se 
+    # Desconfigura a mano el step (por ejemplo poniendo 
+    # driver.step.state = True), esta driver.step() no va a hacer lo
+    # esperado (pensar cómo solucionar).
+    def step(self, duration=1e-6):
+        self.pin_step.pulse(duration)
+    
+
+class M061CS02(abstract.Motor):
+    _STEPS_MODE = (200, 400, 800, 1600, 3200)
+
+    def __init__(self, driver, steps = 200, angle = 0.0):
+        self._driver = driver
+        self._angle = angle
+        self._angle_relative = angle % 360
+        self.steps = steps
+        self._min_angle = 360.0/self.steps
+        self._min_pulse_duration = 1e-6
+
+    def rotate(self, angle, cw):
+        angle_change_sign = (int(cw)*2 - 1)
+        self._driver.direction.state = cw
+        while abs(round(self._angle - angle, 5)) >= self._min_angle:
+            self._driver.step(duration = self._min_pulse_duration)
+            self._angle +=  angle_change_sign * self._min_angle
+        self._angle = round(self._angle, 5)
+
+    def rotate_relative(self, angle, cw, change_angle = True):
+        angle_done = 0.0
+        self._driver.direction.state = cw
+        while abs(round(angle - angle_done, 5)) >= self._min_angle:
+            self._driver.step(duration = self._min_pulse_duration)
+            angle_done += self._min_angle
+        if change_angle:
+            angle_change_sign = (int(cw)*2 - 1)
+            self._angle = round(self._angle + angle_change_sign * angle_done, 5)
+
+    def rotate_step(self, steps, cw, change_angle = True):
+        self._driver.direction.state = cw
+        for _ in range(steps):
+            self._driver.step(duration = self._min_pulse_duration)
+        if change_angle:
+            angle_change_sign = (int(cw)*2 - 1)
+            self._angle = round(self._angle + angle_change_sign * self.min_angle * steps, 5)
+
+    @property
+    def angle(self):
+        return self._angle
+
+    @property
+    def angle_relative(self):
+        return self._angle % 360
+
+    @property
+    def min_angle(self):
+        return 360 / self.steps
+
+    @property
+    def steps(self, steps = 200):
+        return self._STEPS_MODE[self._driver.get_stepping()]
+
+    @steps.setter
+    def steps(self, steps = 200):
+        self._driver.set_stepping(self._STEPS_MODE.index(steps))
+
+    def set_origin(self, angle):
+        self._angle = angle
+
+
+class Spectrometer(abstract.Spectrometer):
+    CALIB_ATTRS = [ '_wl_deg_ratio',
+                    '_greater_wl_cw',
+                    '_max_wl',
+                    '_min_wl',
+                    '_wavelength']
+
+    def __init__(self, motor):
+        self._motor = motor
+        # Es necesario definir todas estas cosas? o directamente ni las defino
+        self._wavelength = None # nm
+        self._greater_wl_cw = None 
+        self._wl_deg_ratio = None # nm/degree
+        self._calibration = None
+        self._max_wl = None
+        self._min_wl = None
+
+    @classmethod
+    def constructor_default(cls, GPIO, MOTOR_DRIVER=A4988, MOTOR=M061CS02):
+        ttls = {
+                'notenable' :   RPTTL(False, ('n', 0), GPIO),
+                'ms1'       :   RPTTL(False, ('n', 1), GPIO),
+                'ms2'       :   RPTTL(False, ('n', 2), GPIO),
+                'ms3'       :   RPTTL(False, ('n', 3), GPIO),
+                'notreset'  :   RPTTL(True , ('n', 4), GPIO),
+                'notsleep'  :   RPTTL(True , ('n', 5), GPIO),
+                'pin_step'  :   RPTTL(False, ('n', 6), GPIO),
+                'direction' :   RPTTL(True , ('n', 7), GPIO),
+                }
+        driver = MOTOR_DRIVER(ttls)
+        motor = MOTOR(driver)
+        return cls(motor)
+
+    # Esto es necesario?
+    def set_wavelength(self, wavelength):
+        self._wavelength = wavelength
+
+    def check_safety(self, wavelength):
+        # este check no parece muy bueno. Pensar cómo mejorarlo
+        if self._min_wl < wavelength < self._max_wl:
+            return True
+        else:
+            return False 
+
+    def goto_wavelength(self, wavelength):
+        angle, cw = self._goto_wavelength(wavelength)
+        if self.check_safety(wavelength):
+            self._motor.rotate_relative(angle, cw)
+
+    def _goto_wavelength(self, wavelength):
+        cw = self._cw_from_wl(wavelength)
+        angle = self._angle_from_wl(wavelength)
+        return angle, cw
+
+    # Tabla de verdad para saber si ir cw o counter cw:
+    # self._greater_wl_cw | wl_dif > 0 | cw
+    # 1 | 1 | 1
+    # 1 | 0 | 0
+    # 0 | 1 | 0
+    # 0 | 0 | 1
+    # Equivalente a (wl_dif > 0) == self._greater_wl_cw
+    def _cw_from_wl(self, wavelength):
+        wl_dif = wavelength - self._wavelength
+        if (wl_dif > 0) == self._greater_wl_cw:
+            return True
+        else:
+            return False
+
+    def _angle_from_wl(self, wavelength, cw):
+        angle = abs(wavelength - self._wavelength)/self._wl_deg_ratio
+        return angle
+
+    def load_calibration(self, path): #wavelength
+        with open(path, 'r') as f:
+            self._calibration = yaml.safe_load(f)
+        for param in self._calibration:
+            setattr(self, "_{}".format(param), self._calibration[param])
+            # Esto hace que la property se agregue a la clase, y no a la
+            # instancia (diferencia entre self y self.__class__). Creo que
+            # En este caso no importa, pero es algo a tener en cuenta
+            setattr(self.__class__, param,
+                    property(fget=lambda self: getattr(self, "_{}".format(param))))
+
+    def _calibration_options(self):
+        print()
+        print("abort: q")
+        print("set max wl: mxwl")
+        print("set min wl: mnwl")
+        print("set wl growth direction: wgd")
+        print("set wl to deg ratio: wdeg")
+        print("set current wl in nm: wl")
+        print("write to file: write")
+        print("print calibration status: s")
+        print("show this message: h")
+        print("\n")
+
+    def _write_calibration_file(self, filename):
+        with open(filename, "w") as f:
+            calibration_dict = self.get_calibration_dict()
+            yaml.dump(calibration_dict, f)
+
+    def status(self):
+        for attr in self.CALIB_ATTRS:
+            if not getattr(self, attr):
+                return False
+        return True
+
+    def get_calibration_dict(self):
+        calibration_dict = {}
+        for attr in self.CALIB_ATTRS:
+            calibration_dict[attr[1:]] = getattr(self, attr)
+        return calibration_dict
+
+    def _wgd_menu(self):
+        print('abort: a')
+        print('rotate clowckwise: cw')
+        print('rotate counterclockwise: ccw')
+        print('rotate n steps: r')
+        print('wavelength grows clockwise: cwg')
+        print('wavelength grows counterclockwise: ccwg')
+
+    def _wgd_is_clockwise(self):
+        cw = None
+        cmd = ''
+        while cmd != 'a':
+            cmd = input('input command: ')
+            if cmd == 'cw':
+                cw = True
+            elif cmd == 'ccw':
+                cw = False
+            elif cmd == 'r':
+                if cw is not None:
+                    steps = input('amount of steps: ')
+                    try:
+                        steps = int(steps)
+                        print(self._motor.angle)
+                        self._motor.rotate_step(steps, cw)
+                        print(self._motor.angle)
+                    except:
+                        print(steps, type(steps))
+                        print("invalid input")
+                        self._wgd_menu()
+                        cmd = ''
+                else:
+                    print("define cw first")
+            elif cmd == 'cwg':
+                return True
+            elif cmd == 'ccwg':
+                return False
+            elif cmd == '' or cmd == 'a':
+                pass
+            else:
+                print("invalid command")
+        return None
+
+    def _wdeg_functionality(self):
+        cw = None
+        cmd = ''
+        cmd = input('input clockwise(t) or counterclockwise(f): ')
+        if cmd == 't':
+            cw = True
+        elif cmd == 'f':
+            cw = False
+        else:
+            print('invalid command')
+            print('aborting...')
+            return None
+        degs = input('input rotation angle (in deg): ')
+        try:
+            degs = float(degs)
+            print(self._motor.angle)
+            self._motor.rotate_relative(degs, cw)
+            print(self._motor.angle)
+        except:
+            print("invalid input")
+            print("aborting...")
+            return None
+        wl_change = input('input wl change in nm: ')
+        try:
+            wl_change = float(wl_change)
+        except:
+            print('invalid input')
+            print('aborting...')
+            return None
+        return wl_change/degs
+
+    def calibrate(self):
+        print("Calibration menu ")
+        cmd = "h"
+        self._calibration_options()
+        while cmd != 'q':
+            print()
+            cmd = input('input command: ')
+            if cmd == 'mxwl':
+                print("input max wavelength value in nm: ")
+                max_wl = input()
+                try:
+                    self._max_wl = float(max_wl)
+                except:
+                    print("invalid input")
+                    cmd = ''
+            elif cmd == 'mnwl':
+                print("input min wavelength value in nm: ")
+                min_wl = input()
+                try:
+                    self._min_wl = float(min_wl)
+                except:
+                    print("invalid input")
+                    cmd = ''
+            elif cmd == 'wgd':
+                self._wgd_menu()
+                self._greater_wl_cw = self._wgd_is_clockwise()
+            elif cmd == 'wdeg':
+                self._wl_deg_ratio = self._wdeg_functionality()
+            elif cmd == 'write':
+                if self.status():
+                    print("Enter calibration full path: \n")
+                    path = input()
+                    self._write_calibration_file(path)
+                else:
+                    print("Complete calibration first")
+            elif cmd == 's':
+                print()
+                for attr in self.CALIB_ATTRS:
+                    print("{}: {}".format(attr[1:], getattr(self, attr)))
+                print("calibration complete: {}".format(self.status()))
+            elif cmd == 'wl':
+                wl = input("input current wl (in nm): ")
+                try:
+                    wl = float(wl)
+                    self.set_wavelength(wl)
+                except:
+                    print("invalid input")
+                    cmd = ''
+            elif cmd == 'h':
+                self._calibration_options()
+            elif cmd == 'q':
+                pass
+            elif cmd == '':
+                pass
+            else:
+                print("invalid command")
+
+class GPIO_helper:
+    def __init__(self, column, pin, io):
+        self.column = column
+        self.pin = pin
+        self.io = io
+        self.state = True
+
+    def write(self, state):
+        self.state = state
+
+    def read(self):
+        return self.state
+
+if __name__ == "__main__":
+    spec = Spectrometer.constructor_default()
+    spec.calibrate()
+    pass
+
+
---
+++ ucnpexp-3.5/abstract.py
@@ -0,0 +1,76 @@
+from abc import ABC, abstractmethod
+import time
+
+
+class Motor(ABC):
+    @abstractmethod
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def rotate(self, angle, cw):
+        pass
+        # informarle al driver cuantos pulsos y direccion
+
+class Spectrometer(ABC):
+    @abstractmethod
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def set_wavelength(self, wavelength):
+        pass
+    
+    @abstractmethod
+    def goto_wavelength(self, wavelength):
+        pass
+
+    @abstractmethod
+    def _goto_wavelength(self, wavelength):
+        pass
+
+
+class TTL:
+
+    def __init__(self, state):
+        self.state = state
+
+    @property
+    def state(self):
+        return self._get_state()
+    
+    @state.setter
+    def state(self, state):
+        self._set_state(state)
+    
+    @abstractmethod
+    def _get_state(self):
+        pass
+       
+    @abstractmethod
+    def _set_state(self, state):
+        pass
+    
+    def toggle(self) -> None:
+        self.state = not self.state
+
+    def pulse(self, duration) -> None:
+        self.toggle()
+        time.sleep(duration)
+        self.toggle()
+    
+    def __str__(self):
+        return str(self.state)
+    
+
+class MotorDriver(ABC):
+
+    @abstractmethod
+    def __init__(self):
+        pass
+    
+    @abstractmethod
+    def step(self):
+        pass
+
+
